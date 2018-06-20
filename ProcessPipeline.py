756f80762627aa3362f49ece58248cbb7f400c91
milan <janosovm@gmail.com>
2018-06-20 14:11:51 +0100

    up

--- ProcessPipeline.py
+++ ProcessPipeline.py
@@ -81,8 +81,12 @@ if sys.argv[2] == 'preproc':
     users_tips    = ParseJsons.get_tips_locations_and_users(    unknown_users, local_users, city, bbox, inroot, outroot, users_homes)
 
     ParseJsons.write_home_locations( users_homes, city, outroot,  len(users_likes.keys()))
+    ParseJsons.get_users_coordinates(nonlocal_users, users_likes, users_tips, users_photos, city, outroot)
     ParseJsons.get_users_distance_distr_from_home(city, outroot) 
     ParseJsons.get_users_venues(unknown_users, local_users, users_photos, users_likes, users_tips, city, outroot)
+
+
+
 
     ParseJsons.venues_distance_mtx(bbox, city, outroot)
     ParseJsons.get_venues_users(city, outroot)       
