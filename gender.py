670c2b62e7614121f2cb452e087209fa855f57ee
milan <janosovm@gmail.com>
2018-12-15 20:52:23 +0100

    up

--- gender.py
+++ gender.py
@@ -6,7 +6,7 @@ city = sys.argv[1]
 
 fout = open('../ProcessedData/london/user_info/users_gender.dat', 'w')
 
-for ind, line in enumerate(open('../Data/fsqdb/london/'+city+'_users.json')):
+for ind, line in enumerate(open('../Data/fsqdb/'+city+'/'+city+'_users.json')):
 
     #if ind == 2 : break
 
